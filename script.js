0260a8c0f5203937b77d382fd2cdc8555d8ebea5
Pat Myron <pmyron09@gmail.com>
2018-02-04 21:25:29 -0800

    sorting graph

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function param(name) {
     return (location.search.split(name + '=')[1] || '').split('&')[0];
 }
+function sortMap(mapName) {
+    return _.chain(mapName)
+        .map((val, key) => {
+            return { name: key, count: val }
+        })
+        .sortBy('count')
+        .reverse()
+        .keyBy('name')
+        .mapValues('count')
+        .value();
+}
