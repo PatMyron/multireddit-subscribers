600616005d0473e1632d1750ed9bf40103ef573d
Pat Myron <pmyron@nd.edu>
2019-08-18 17:17:20 -0700

    moving inline script to script.js and deferring

--- script.js
+++ script.js
@@ -13,3 +13,51 @@ function sortMap(mapName) {
         .mapValues('count')
         .value();
 }
+
+reddit.multi(param('multi')).fetch(function (res) {
+    teams = res.data.subreddits.map(x => x.name);
+    teamSubs = {};
+    ajaxCallsRemaining = teams.length;
+    teams.forEach(function (team) {
+        reddit.about(team).fetch(function (res2) {
+            teamSubs[team] = res2.data.subscribers;
+            ajaxCallsRemaining--;
+            if (ajaxCallsRemaining <= 0) {
+                document.title = 'Multireddit Subscribers | ' + param('multi').split('/').pop();
+                $('#otherStuff').remove();
+                teamSubs = sortMap(teamSubs);
+
+                ctx = document.getElementById("myChart").getContext('2d');
+                myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: _.keys(teamSubs),
+                        datasets: [{
+                            label: '# of Subscribers',
+                            data: _.values(teamSubs)
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        scales: {
+                            xAxes: [{
+                                gridLines: {
+                                    display: false
+                                },
+                                ticks: {
+                                    autoSkip: false,
+                                }
+                            }],
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+            }
+        })
+    });
+});
